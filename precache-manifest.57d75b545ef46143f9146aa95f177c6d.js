57cc2f225ee6d1e71c639326a232bf556e235969
armedev <arbazcr77@gmail.com>
2020-04-30 15:02:58 +0530

    Updates

---
+++ precache-manifest.57d75b545ef46143f9146aa95f177c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a430ff0d396b93a81775a345a2b6ed74",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "54da8b1809925ae091bc",
+    "url": "/monsters/static/css/main.fe1ab792.chunk.css"
+  },
+  {
+    "revision": "bf96d8e81dd82ac5f2f1",
+    "url": "/monsters/static/js/2.efce2fcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.efce2fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54da8b1809925ae091bc",
+    "url": "/monsters/static/js/main.8c89e612.chunk.js"
+  },
+  {
+    "revision": "26a227e52a9c00275403",
+    "url": "/monsters/static/js/runtime-main.eed986be.js"
+  }
+]);
